bd4d229749319e4dcdf39328ce27ee89b0d58e3d
DESKTOP-8EG4PPP\BAP <hoangviet.hung.009@gmail.com>
2021-01-06 13:17:52 +0700

    update index page

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,22 @@
 const initHeroBanner = () => {
 	let heroBanner = new Swiper(".hero-banner .swiper-container");
 };
+const initIndexBestSeller = () => {
+	let indexBestSeller = new Swiper(
+		".slider-index-best-seller .swiper-container",
+		{
+			slidesPerView: 3,
+			navigation: {
+				nextEl:
+					".slider-index-best-seller .button-navigation-slider.next",
+				prevEl:
+					".slider-index-best-seller .button-navigation-slider.prev",
+			},
+		}
+	);
+};
 
 document.addEventListener("DOMContentLoaded", () => {
 	initHeroBanner();
+	initIndexBestSeller();
 });
