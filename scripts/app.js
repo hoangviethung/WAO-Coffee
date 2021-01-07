dc35fba0028c2d10014d9a3b79850eb54beec27f
hoangviethung <hoangviet.hung.009@gmail.com>
2021-01-08 03:33:57 +0700

    index

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,36 @@
+const fixedHeaderWhenScroll = () => {
+	const header = document.querySelector("header");
+	if (window.pageYOffset > header.offsetHeight) {
+		header.classList.add("fixed");
+	} else {
+		header.classList.remove("fixed");
+	}
+};
+const initializeSubMenu = () => {
+	const itemsNaviation = document.querySelectorAll("nav ul li");
+	itemsNaviation.forEach((item) => {
+		const sub = item.querySelector("ul");
+		if (sub) {
+			item.classList.add("has-sub");
+		}
+	});
+};
+const showMenuMobile = () => {
+	const btn = document.querySelector(".toggle-menu-mobile");
+	const close = document.querySelector(".close-menu-mobile");
+	const navigation = document.querySelector("nav");
+	// CHECK BUTTON
+	if (btn) {
+		btn.addEventListener("click", () => {
+			navigation.classList.add("active");
+		});
+	}
+	if (close) {
+		close.addEventListener("click", () => {
+			navigation.classList.remove("active");
+		});
+	}
+};
 const initHeroBanner = () => {
 	let heroBanner = new Swiper(".hero-banner .swiper-container");
 };
@@ -25,9 +58,55 @@ const initIndexRecipe = () => {
 		},
 	});
 };
+const initIndexCustomer = () => {
+	let indexCustomer = new Swiper(".slider-index-customer .swiper-container", {
+		slidesPerView: 5,
+		spaceBetween: 35,
+		navigation: {
+			nextEl: ".slider-index-customer .button-navigation-slider.next",
+			prevEl: ".slider-index-customer .button-navigation-slider.prev",
+		},
+	});
+};
+const initIndexNews = () => {
+	let indexNews = new Swiper(".slider-index-news .swiper-container", {
+		slidesPerView: 3,
+		spaceBetween: 35,
+		navigation: {
+			nextEl: ".slider-index-news .button-navigation-slider.next",
+			prevEl: ".slider-index-news .button-navigation-slider.prev",
+		},
+	});
+};
+const initializeWowJs = () => {
+	var wow = new WOW({
+		boxClass: "wow", // animated element css class (default is wow)
+		animateClass: "animated", // animation css class (default is animated)
+		offset: 0, // distance to the element when triggering the animation (default is 0)
+		mobile: true, // trigger animations on mobile devices (default is true)
+		live: true, // act on asynchronously loaded content (default is true)
+		callback: function (box) {
+			// the callback is fired every time an animation is started
+			// the argument that is passed in is the DOM node being animated
+		},
+		scrollContainer: null, // optional scroll container selector, otherwise use window,
+		resetAnimation: true, // reset animation on end (default is true)
+	});
+	wow.init();
+};
 
 document.addEventListener("DOMContentLoaded", () => {
+	fixedHeaderWhenScroll();
+	showMenuMobile();
+	initializeSubMenu();
 	initHeroBanner();
 	initIndexBestSeller();
 	initIndexRecipe();
+	initIndexCustomer();
+	initIndexNews();
+	initializeWowJs();
+});
+
+window.addEventListener("scroll", () => {
+	fixedHeaderWhenScroll();
 });
